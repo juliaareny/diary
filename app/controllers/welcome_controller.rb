b1b135a4dc7cb221a2b4bee6a6d72089c7da4174
Júlia <julia@getquipu.com>
2015-03-29 12:02:18 +0200

    create an entry model

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
   def index
+    @entries=Entry.all
+    
+  	@websites = [
+			["http://www.getquipu.com", "Get Quipu NOW!"],
+			["http://www.bellonch.com", "xxx"]
+				]
+
   end
 end
